19c691b7362449dc16ef411d1a6968940fb7177c
marisa <marisaviotti@gmail.com>
2025-09-22 16:45:42 -0300

    feat: mejorar estilos de componentes y actualizar colores en Footer, Header y Contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';  
 function Footer() {  
     return (  
-        <footer style={{ backgroundColor: "#333", padding: "10px", textAlign: "center", marginTop: "20px" }}>  
+        <footer style={{ backgroundColor: "#333", color: "white", padding: "10px", textAlign: "center", marginTop: "20px" }}>  
             <p>&copy; 2025 - Shipping Star Solutions LLC. All rights reserved.</p>           
         </footer>  
     );  
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';  
 function Header() {  
     return (  
-        <header style={{ backgroundColor: "#344535ff", padding: "10px", textAlign: "center", color: "white" }}>  
+        <header style={{ background: "#4169e1", padding: "10px", textAlign: "center", color: "white" }}>  
             <h1>SHIPPING START SOLUTIONS LLC</h1>
             <p><b>Your trusted partner in business solutions.</b></p>  
         </header>  
