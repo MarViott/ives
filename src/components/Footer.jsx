80c6aefeea29b6131968b1a7e55148fcb62707a8
marisa <marisaviotti@gmail.com>
2025-10-21 17:04:17 -0300

    footer completo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';  
 function Footer() {  
-    return (  
-        <footer style={{ backgroundColor: "#333", color: "white", padding: "10px", textAlign: "center", marginTop: "20px" }}>  
-            <p>&copy; 2025 - Shipping Star Solutions LLC. All rights reserved.</p>           
-        </footer>  
+    return (
+      <footer
+        style={{
+          backgroundColor: "#333",
+          color: "white",
+          padding: "10px",
+          textAlign: "center",
+          marginTop: "20px",
+        }}
+      >
+        <p>
+          &copy; 2025 - Shipping Star Solutions LLC. All rights reserved -
+          Desrollado por
+          <a
+            href="https://www.linkedin.com/in/marisa-viotti-74601036a/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#0077b5", textDecoration: "none" }}
+          >
+            Marisa Viotti
+          </a></p>
+        
+      </footer>
     );  
 }  
 
